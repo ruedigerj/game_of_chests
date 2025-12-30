0736f7f088f18335d118c82ef01de726e84c8c45
ruedigerj <88975967+ruedigerj@users.noreply.github.com>
2025-12-30 19:21:08 +0100

    const inGame = (false);

--- app.js
+++ app.js
@@ -481,7 +481,7 @@ function renderState(state){
   // Refresh button: disabled during a game (consider 'offering' and 'placing' as in-game)
   if(refreshBtn) {
     // const inGame = (phase === 'offering' || phase === 'placing');
-    const inGame = (phase === 'placing');
+    const inGame = (false);
     refreshBtn.disabled = inGame;
   }
 
